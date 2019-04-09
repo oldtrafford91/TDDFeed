6af61844a58f01603ebca60469dc6f33adbe8c04
Hai Tran <tuanhai.dev@gmail.com>
2019-04-10 05:47:29 +0700

    Refactor test using factory pattern

--- TDDFeedTests/RemoteFeedLoaderTest.swift
+++ TDDFeedTests/RemoteFeedLoaderTest.swift
@@ -16,7 +16,30 @@ protocol HTTPClient {
   func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
+class RemoteFeedLoaderTest: XCTestCase {
+  func test_doesNotRequestDataFromURL() {
+    let (client, _) = makeSUT()
+    XCTAssertNil(client.requestedURL)
+  }
+  
+  func test_load_requestDataFromURL() {
+    let url = URL(string: "https://example.com")!
+    let (client, sut) = makeSUT()
+    sut.load()
+    
+    XCTAssertEqual(client.requestedURL, url)
+  }
+}
+
+// MARK: Helpers
+
+private func makeSUT(url: URL = URL(string: "https://example.com")!) -> (client: HTTPClientSpy, loader: RemoteFeedLoader) {
+  let client = HTTPClientSpy()
+  let sut = RemoteFeedLoader(url: url, client: client)
+  return (client, sut)
+}
+
+private class HTTPClientSpy: HTTPClient {
   var requestedURL: URL?
   
   func get(from url: URL) {
@@ -24,21 +47,4 @@ class HTTPClientSpy: HTTPClient {
   }
 }
 
-class RemoteFeedLoaderTest: XCTestCase {
-  func test_doesNotRequestDataFromURL() {
-    let url = URL(string: "aurl")!
-    let client = HTTPClientSpy()
-    _ = RemoteFeedLoader(url: url, client: client)
-    XCTAssertNil(client.requestedURL)
-  }
-  
-  func test_load_requestDataFromURL() {
-    let url = URL(string: "aurl")!
-    let client = HTTPClientSpy()
-    let sut = RemoteFeedLoader(url: url, client: client)
-    
-    sut.load()
-    
-    XCTAssertEqual(client.requestedURL, url)
-  }
-}
+
