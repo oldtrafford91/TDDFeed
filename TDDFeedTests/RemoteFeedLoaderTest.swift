1968e9e3bcceb53b55ccbe362f416808d1738a95
Hai Tran <tuanhai.dev@gmail.com>
2019-04-11 06:24:19 +0700

    Move `RemoteFeedLoader` component to production

--- TDDFeedTests/RemoteFeedLoaderTest.swift
+++ TDDFeedTests/RemoteFeedLoaderTest.swift
@@ -1,20 +1,6 @@
 import XCTest
 
-class RemoteFeedLoader {
-  let url: URL
-  let client: HTTPClient
-  init(url: URL, client: HTTPClient) {
-    self.url = url
-    self.client = client
-  }
-  func load() {
-    client.get(from: url)
-  }
-}
-
-protocol HTTPClient {
-  func get(from url: URL)
-}
+import TDDFeed
 
 class RemoteFeedLoaderTest: XCTestCase {
   func test_doesNotRequestDataFromURL() {
