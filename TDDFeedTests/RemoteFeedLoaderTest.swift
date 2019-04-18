612e955c24d05d8b62230eb0d419895c92038737
Hai Tran <tuanhai.dev@gmail.com>
2019-04-18 23:13:32 +0700

    Add memory leaks detection

--- TDDFeedTests/RemoteFeedLoaderTest.swift
+++ TDDFeedTests/RemoteFeedLoaderTest.swift
@@ -83,43 +83,50 @@ class RemoteFeedLoaderTest: XCTestCase {
       client.complete(withStatusCode: 200, data: jsonData)
     })
   }
-}
-
-// MARK: Helpers
-
-private func makeSUT(url: URL = URL(string: "https://example.com")!) -> (client: HTTPClientSpy, loader: RemoteFeedLoader) {
-  let client = HTTPClientSpy()
-  let sut = RemoteFeedLoader(url: url, client: client)
-  return (client, sut)
-}
-
-private func expect(_ sut: RemoteFeedLoader, completeWith result: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
-  var capturedResults = [RemoteFeedLoader.Result]()
-  sut.load {
-    capturedResults.append($0)
+  
+  // MARK: Helpers
+  private func makeSUT(url: URL = URL(string: "https://example.com")!, file: StaticString = #file, line: UInt = #line) -> (client: HTTPClientSpy, loader: RemoteFeedLoader) {
+    let client = HTTPClientSpy()
+    let sut = RemoteFeedLoader(url: url, client: client)
+    trackMemoryLeaks(sut)
+    trackMemoryLeaks(client)
+    return (client, sut)
   }
-  action()
-  XCTAssertEqual(capturedResults, [result], file: file, line: line)
-}
-
-private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedItem, json: [String: Any]) {
-  let item = FeedItem(id: id, description: description, location: location, imageURL: imageURL)
-  let json = [
-    "id": item.id.uuidString,
-    "description": item.description,
-    "location": item.location,
-    "image": item.imageURL.absoluteString
-    ].reduce(into: [String: Any]()) { (acc, e) in
-      if let value = e.value {
-        acc[e.key] = value
-      }
+  
+  private func trackMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line ) {
+    addTeardownBlock { [weak instance] in
+      XCTAssertNil(instance, "Instance of sut should be deallocated. Potential memory leak.")
+    }
   }
-  return (item, json)
-}
-
-private func makeItemsJSONData(_ items: [[String: Any]]) -> Data {
-  let json = ["items": items]
-  return try! JSONSerialization.data(withJSONObject: json)
+  
+  private func expect(_ sut: RemoteFeedLoader, completeWith result: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+    var capturedResults = [RemoteFeedLoader.Result]()
+    sut.load {
+      capturedResults.append($0)
+    }
+    action()
+    XCTAssertEqual(capturedResults, [result], file: file, line: line)
+  }
+  
+  private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedItem, json: [String: Any]) {
+    let item = FeedItem(id: id, description: description, location: location, imageURL: imageURL)
+    let json = [
+      "id": item.id.uuidString,
+      "description": item.description,
+      "location": item.location,
+      "image": item.imageURL.absoluteString
+      ].reduce(into: [String: Any]()) { (acc, e) in
+        if let value = e.value {
+          acc[e.key] = value
+        }
+    }
+    return (item, json)
+  }
+  
+  private func makeItemsJSONData(_ items: [[String: Any]]) -> Data {
+    let json = ["items": items]
+    return try! JSONSerialization.data(withJSONObject: json)
+  }
 }
 
 private class HTTPClientSpy: HTTPClient {
